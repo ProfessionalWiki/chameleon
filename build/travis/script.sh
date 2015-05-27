4c1a4b808b059d87a1434724a1963b1250ba6e71
Stephan Gambke <s7eph4n@gmail.com>
2015-05-27 23:18:50 +0200

    Work around T100409 (mediawiki release tags vanished)
    WMF removed release tags from mediawiki github and there has been no indication
    (https://phabricator.wikimedia.org/T100409) whether this is going to be solved
    or a permanent state therefore we try to match tags ourselves
    
    Change-Id: I0e20cfd814fbe603e7c1beef2375c4aa46be6c1a

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -29,11 +29,21 @@ set -x
 originalDirectory=$(pwd)
 
 function installMediaWiki {
+
+	## WMF removed release tags from mediawiki github and there has been no
+	## indication (https://phabricator.wikimedia.org/T100409) whether this is going
+	## to be solved or a permanent state therefore we try to match tags ourselves
+	case "$MW" in
+	'1.22.1')
+	  HASH='11ceb42'
+	  ;;
+	esac
+
 	cd ..
 
-	wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
+	wget https://github.com/wikimedia/mediawiki/archive/$HASH.tar.gz -O $MW.tar.gz
 	tar -zxf $MW.tar.gz
-	mv mediawiki-$MW mw
+	mv mediawiki-* mw
 
 	cd mw
 

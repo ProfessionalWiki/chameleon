7f9ca6c47df86e843d8a1425d4570eaf6a405165
Morne Alberts <morne@malberts.net>
2021-06-18 18:43:20 +0200

    Add navbar toggler text support

--- src/Components/NavbarHorizontal.php
+++ src/Components/NavbarHorizontal.php
@@ -260,7 +260,7 @@ class NavbarHorizontal extends Component {
 
 		return $this->indent() .
 			'<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#' . $id .
-			'"></button>' .
+			'">' . $this->getTogglerText() . '</button>' .
 			IdRegistry::getRegistry()->element( 'div', [ 'class' => 'collapse navbar-collapse',
 			'id' => $id ], $tail, $this->indent() );
 	}
@@ -270,6 +270,17 @@ class NavbarHorizontal extends Component {
 	 */
 	protected function isCollapsible() {
 		return filter_var( $this->getAttribute( 'collapsible', 'true' ), FILTER_VALIDATE_BOOLEAN );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getTogglerText() {
+		if ( !filter_var( $this->getAttribute( 'showTogglerText', 'false' ), FILTER_VALIDATE_BOOLEAN ) ) {
+			return '';
+		}
+		return \Html::rawElement( 'span', [ 'class' => 'navbar-toggler-text' ],
+			$this->getSkinTemplate()->getMsg( 'chameleon-toggler' )->escaped() );
 	}
 
 }
